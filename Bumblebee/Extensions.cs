f8cafb4adb55aa5d2fe4c98c15f8bc429ca5ab24
Patrick Herrmann <patrickwherrmann@gmail.com>
2012-12-14 13:21:24 -0800

    Refactor namespaces

--- Bumblebee/Extensions.cs
+++ Bumblebee/Extensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Bumblebee.Implementation;
 using Bumblebee.Interfaces;
 using OpenQA.Selenium;
 
@@ -34,10 +33,56 @@ namespace Bumblebee
         {
             return options.Single(option => option.Selected);
         }
+    }
 
-        public static T Random<T>(this IEnumerable<T> enumerable)
+    public static class Randomization
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
-            return enumerable.OrderBy(id => new Guid()).First();
+            var rng = new Random();
+            return source.Shuffle(rng);
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (rng == null) throw new ArgumentNullException("rng");
+
+            return source.ShuffleIterator(rng);
+        }
+
+        private static IEnumerable<T> ShuffleIterator<T>(
+            this IEnumerable<T> source, Random rng)
+        {
+            var buffer = source.ToList();
+
+            for (var i = 0; i < buffer.Count; i++)
+            {
+                var j = rng.Next(i, buffer.Count);
+                yield return buffer[j];
+
+                buffer[j] = buffer[i];
+            }
+        }
+
+        public static T Random<T>(this IEnumerable<T> source)
+        {
+            var rng = new Random();
+            return source.Random(rng);
+        }
+
+        public static T Random<T>(this IEnumerable<T> source, Random rng)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (rng == null) throw new ArgumentNullException("rng");
+
+            return RandomIterator(source, rng);
+        }
+
+        private static T RandomIterator<T>(this IEnumerable<T> source, Random rng)
+        {
+            var buffer = source as IList<T> ?? source.ToList();
+            return buffer[rng.Next(buffer.Count)];
         }
     }
 
