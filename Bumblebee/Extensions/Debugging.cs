67bb3bcf61314b8469e992bff30db93b231c3bb5
Patrick Herrmann <patrickwherrmann@gmail.com>
2013-03-18 16:41:50 -0700

    Add PlaySound as a debugging 'tool'

--- Bumblebee/Extensions/Debugging.cs
+++ Bumblebee/Extensions/Debugging.cs
@@ -23,6 +23,12 @@ namespace Bumblebee.Extensions
             return obj;
         }
 
+        public static T PlaySound<T>(this T obj, int pause = 0)
+        {
+            System.Media.SystemSounds.Exclamation.Play();
+            return obj.Pause(pause);
+        }
+
         public static T Pause<T>(this T block, int seconds)
         {
             if (seconds > 0) Thread.Sleep(1000 * seconds);
