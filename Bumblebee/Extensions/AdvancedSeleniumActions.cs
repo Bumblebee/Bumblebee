92200e56f54e8401206364f204b158f71f9674bc
Patrick Herrmann <patrickwherrmann@gmail.com>
2013-04-05 08:12:06 -0700

    Put drag action class in same file as drag extension methods

--- Bumblebee/Extensions/AdvancedSeleniumActions.cs
+++ Bumblebee/Extensions/AdvancedSeleniumActions.cs
@@ -19,4 +19,56 @@ namespace Bumblebee.Extensions
             return new DragAction<TParent>(parent, getDraggable);
         }
     }
+
+    public class DragAction<TParent> where TParent : IBlock
+    {
+        private TParent Parent { get; set; }
+        private IDraggable Draggable { get; set; }
+
+        public DragAction(TParent parent, Func<TParent, IDraggable> getDraggable)
+        {
+            Parent = parent;
+            Draggable = getDraggable.Invoke(parent);
+        }
+
+        public TParent AndDrop(Func<TParent, IHasBackingElement> getDropzone)
+        {
+            PerformDragAndDrop(getDropzone);
+
+            return Parent.Session.CurrentBlock<TParent>();
+        }
+
+        public TCustomResult AndDrop<TCustomResult>(Func<TParent, IHasBackingElement> getDropzone) where TCustomResult : IBlock
+        {
+            PerformDragAndDrop(getDropzone);
+
+            return Parent.Session.CurrentBlock<TCustomResult>();
+        }
+
+        public TParent AndDrop(int x, int y)
+        {
+            PerformDragAndDrop(x, y);
+
+            return Parent.Session.CurrentBlock<TParent>();
+        }
+
+        public TCustomResult AndDrop<TCustomResult>(int x, int y) where TCustomResult : IBlock
+        {
+            PerformDragAndDrop(x, y);
+
+            return Parent.Session.CurrentBlock<TCustomResult>();
+        }
+
+        private void PerformDragAndDrop(Func<TParent, IHasBackingElement> getDropzone)
+        {
+            var dropzone = getDropzone.Invoke(Parent);
+
+            Parent.GetDragAndDropPerformer().DragAndDrop(Draggable.Tag, dropzone.Tag);
+        }
+
+        private void PerformDragAndDrop(int x, int y)
+        {
+            Parent.GetDragAndDropPerformer().DragAndDrop(Draggable.Tag, x, y);
+        }
+    }
 }
