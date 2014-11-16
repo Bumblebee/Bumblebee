dc082954ce968bde855c26171a393116181ab9c2
Todd Meinershagen <todd@meinershagen.net>
2014-11-16 08:41:23 -0600

    Modified tests for readability and to use the new VerifyThat() method to use FluentAssertions.

--- Bumblebee.Examples.Web.IntegrationTests/RedditTests.cs
+++ Bumblebee.Examples.Web.IntegrationTests/RedditTests.cs
@@ -4,6 +4,7 @@ using Bumblebee.Examples.Web.IntegrationTests.Infrastructure;
 using Bumblebee.Examples.Web.Pages;
 using Bumblebee.Extensions;
 using Bumblebee.Setup;
+using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 
@@ -56,10 +57,14 @@ namespace Bumblebee.Examples.Web.IntegrationTests
         {
             Threaded<Session>
                 .CurrentBlock<LoggedOutPage>()
-                .Verify("that there is at least one TIL on front page",
-                    page => page.Posts.Any(post => post.Subreddit.Text.Contains("todayilearned")))
-                .Verify("there are no selenium subreddit posts on front page",
-                    page => page.Posts.All(post => !post.Subreddit.Text.Contains("selenium")));
+                .VerifyThat(page => 
+                    page.Posts.Any(post =>
+                    post.Subreddit.Text.Contains("todayilearned"))
+                    .Should().BeTrue("there should be at least one til on the front page"))
+                .VerifyThat(page => 
+                    page.Posts.Any(post =>
+                    post.Subreddit.Text.Contains("selenium"))
+                    .Should().BeFalse("there should be no selenium subreddit posts on the front page"));
         }
 
         [Test]
@@ -67,11 +72,21 @@ namespace Bumblebee.Examples.Web.IntegrationTests
         {
             Threaded<Session>
                 .CurrentBlock<LoggedOutPage>()
-                .FeaturedSubreddits.First(featured => featured.Text == "BOOKS").Click()
-                .DebugPrint(page => page.RankedPosts.Select(post => post.Title.Text))
-                .Verify("that first page starts with rank of 1", page => page.RankedPosts.First().Rank == "1")
+                .FeaturedSubreddits.First(featured =>
+                    featured.Text == "BOOKS")
+                .Click()
+                .DebugPrint(page =>
+                    page.RankedPosts
+                        .Select(post => post.Title.Text))
+                .VerifyThat(page =>
+                    page.RankedPosts
+                        .First().Rank
+                        .Should().Be("1", "the first page should start with a rank of 1"))
                 .Next.Click()
-                .Verify("that second page starts with rank of 26", page => page.RankedPosts.First().Rank == "26");
+                .VerifyThat(page =>
+                    page.RankedPosts
+                        .First().Rank
+                        .Should().Be("26", "the second page should start with a rank of 26"));
         }
     }
 }
