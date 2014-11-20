1fb3ec1c1eba203c4b579c4a37db7bf2bc831433
cdroulers <droulers.christian@gmail.com>
2014-11-20 16:08:48 -0500

    Add NuGet stuff.

--- Bumblebee.KendoUI/Properties/AssemblyInfo.cs
+++ Bumblebee.KendoUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bumblebee.KendoUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bumblebee")]
+[assembly: AssemblyDescription("Implementation of certain controls for the Kendo UI library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bumblebee.KendoUI")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Patrick Herrmann, John Hammerlund, Todd Meinershagen, Christian Droulers")]
+[assembly: AssemblyProduct("Bumblebee")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b039576-ca2a-4818-83b1-c04ca4aea146")]
+[assembly: Guid("f63d1cf0-ff82-47a2-9d3a-c2f99d5a993c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: InternalsVisibleTo("Bumblebee.IntegrationTests")]
