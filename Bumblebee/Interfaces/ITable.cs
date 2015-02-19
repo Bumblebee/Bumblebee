ff1d95f709be1402b64cb21b7efd71343ff134c2
Dru Sellers <dru@drusellers.com>
2015-02-19 11:33:21 -0600

    fixing up some merge issues

--- Bumblebee/Interfaces/ITable.cs
+++ Bumblebee/Interfaces/ITable.cs
@@ -9,8 +9,8 @@ namespace Bumblebee.Interfaces
         IEnumerable<string> Headers { get; }
         IEnumerable<ITableRow> Rows { get; }
         IEnumerable<string> Footers { get; }
-        T HeaderAs<T>() where T : Element;
-        IEnumerable<T> RowsAs<T>() where T : Element;
-        T FooterAs<T>() where T : Element;
+        T HeaderAs<T>() where T : SpecificBlock;
+        IEnumerable<T> RowsAs<T>() where T : SpecificBlock;
+        T FooterAs<T>() where T : SpecificBlock;
     }
 }
