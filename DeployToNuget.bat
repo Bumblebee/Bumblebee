ad1d4f0ee774fe0cd119c1de50898c04b865ebb3
Todd Meinershagen <todd@meinershagen.net>
2015-09-10 13:53:39 -0500

    Updated the nuget push bat file.

--- DeployToNuget.bat
+++ DeployToNuget.bat
@@ -27,5 +27,5 @@ nuget.exe pack -Build -OutputDirectory build %package% -Prop Configuration=Relea
 REM Push to Nuget 
 REM =============
 cd build
-nuget.exe push *.nupkg
+..\nuget.exe push *.nupkg
 cd ..
