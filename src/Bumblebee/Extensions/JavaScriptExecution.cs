6da027095d8dbfcd838de9835a876bd01c308ed9
Todd Meinershagen <todd@meinershagen.net>
2015-10-04 03:06:34 -0500

    Marked some obsolete methods as error and added back the SpecificBlock type with obsolete marking so that users will not be confused when it is no longer supported.  This should address issue #65.

--- src/Bumblebee/Extensions/JavaScriptExecution.cs
+++ src/Bumblebee/Extensions/JavaScriptExecution.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 
 using OpenQA.Selenium;
+using OpenQA.Selenium.Safari.Internal;
 using OpenQA.Selenium.Support.Extensions;
 
 namespace Bumblebee.Extensions
 {
 	public static class JavaScriptExecution
 	{
-		[Obsolete("Please use the Selenium built-in function OpenQA.Selenium.Support.Extensions.WebDriverExtensions.ExecuteJavaScript, or call the ExecuteJavaScript method directly off of the Session object.")]
+		[Obsolete("Please use the Selenium built-in function OpenQA.Selenium.Support.Extensions.WebDriverExtensions.ExecuteJavaScript, or call the ExecuteJavaScript method directly off of the Session object.", error: true)]
 		public static T ExecuteScript<T>(this IWebDriver driver, string script, params object[] args)
 		{
 			return driver.ExecuteJavaScript<T>(script, args);
 		}
 
+		// TODO:  Should this be marked as error?
+		// TODO:  Should we introduce the FindElementsByJQuery() as a convenience method?
 		[Obsolete("Please use the new By.JQuery functionality provided.")]
 		public static IEnumerable<IWebElement> GetElementsByJQuery(this IWebDriver driver, string query)
 		{
