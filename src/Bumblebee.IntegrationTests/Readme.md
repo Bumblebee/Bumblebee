01387b01f2fadc4d8bfaf77f135c3ad1928362cc
Todd Meinershagen <todd@meinershagen.net>
2016-09-18 14:50:53 -0500

    Optimization - moving Nancy host logic to single assembly set up.  Also, configured project to run in parallel at the fixture level.

--- src/Bumblebee.IntegrationTests/Readme.md
+++ src/Bumblebee.IntegrationTests/Readme.md
@@ -3,3 +3,9 @@
 1.  Install 7-zip
 2.  Download proper ChromeDriver version for your machine from http://chromedriver.storage.googleapis.com/[x.xx]/chromedriver_win32.zip.
 3.  Extract the file (chromedriver.exe) and place in a folder that is on your path.  (Recommend:  C:\tools\WebDriver)
+
+## Example Powershell for Downloading
+
+$zipPath = "$env:USERPROFILE\chromedriver_win32.zip"
+(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/2.24/chromedriver_win32.zip', $zipPath)
+.\7z.exe x $zipPath -oC:\Tools\WebDriver -aoa
--- src/Bumblebee.IntegrationTests/Shared/Hosting/HostTestFixture.cs
+++ src/Bumblebee.IntegrationTests/Shared/Hosting/HostTestFixture.cs
@@ -7,35 +7,11 @@ namespace Bumblebee.IntegrationTests.Shared.Hosting
 	[TestFixture]
 	public abstract class HostTestFixture
 	{
-		private IHost _host;
-
-		protected HostTestFixture() : this("http://localhost:5000")
-		{
-		}
-
-		protected HostTestFixture(string baseUrl)
-		{
-			BaseUrl = baseUrl;
-		}
-
-		[OneTimeSetUp]
-		public void Init()
-		{
-			_host = new Host(new Uri(BaseUrl));
-			_host.Start();
-		}
-
-		[OneTimeTearDown]
-		public void Dispose()
-		{
-			_host.Stop();
-		}
-
 		protected virtual string GetUrl(string page)
 		{
 			return String.Format("{0}/Content/{1}", BaseUrl, page);
 		}
 
-		protected string BaseUrl { get; private set; }
-	}
+		public static string BaseUrl => "http://localhost:5000";
+    }
 }
