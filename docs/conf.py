06a146b80b8c855d2d33c3467fcdb1dafe183876
Todd Meinershagen <todd@meinershagen.net>
2018-03-19 22:33:17 -0500

    Updating the config file.

--- docs/conf.py
+++ docs/conf.py
@@ -16,16 +16,18 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+
 # -- Project information -----------------------------------------------------
 
 project = 'Bumblebee'
-copyright = '2012, Todd Meinershagen, Patrick Herrmann, and John Hammerlund'
+copyright = '2018, Todd Meinershagen'
 author = 'Todd Meinershagen'
 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = ''
+release = 'v1.2.0'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,7 +38,8 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,13 +47,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# from recommonmark.parser import CommonMarkParser
-
-# source_parsers = {
-#     '.md': CommonMarkParser,
-# }
-
 source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -65,7 +63,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
