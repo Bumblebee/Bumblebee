7204253f3822525283ae4a194f1138120e556c88
Todd Meinershagen <todd@meinershagen.net>
2018-03-21 02:39:51 -0500

    Fixing test for appveyor.

--- src/Bumblebee.IntegrationTests/Setup/SessionTests/given_generic_environment_and_custom_settings_when_capturing.cs
+++ src/Bumblebee.IntegrationTests/Setup/SessionTests/given_generic_environment_and_custom_settings_when_capturing.cs
@@ -14,49 +14,51 @@ using NUnit.Framework;
 
 namespace Bumblebee.IntegrationTests.Setup.SessionTests
 {
-	[TestFixture]
-	public class Given_generic_environment_and_custom_settings_When_capturing : HostTestFixture
-	{
-		private string _filePath;
-		private Session _session;
-		private Session _returnSession;
+    [TestFixture]
+    public class Given_environment_and_custom_settings_When_capturing<T> : HostTestFixture
+        where T : IDriverEnvironment, new()
+    {
+        private string _filePath;
+        private Session _session;
+        private Session _returnSession;
 
-		[OneTimeSetUp]
-		public void Before()
-		{
-			var currentMethod = CallStack.GetCurrentMethod().GetFullName();
+        [OneTimeSetUp]
+        public void Before()
+        {
+            var currentMethod = CallStack.GetCurrentMethod().GetFullName();
 
-			var path = Path.GetTempPath();
-			_filePath = Path.ChangeExtension(Path.Combine(path, currentMethod), "png");
-			File.Delete(_filePath);
+            var path = Path.GetTempPath();
+            _filePath = Path.ChangeExtension(Path.Combine(path, currentMethod), "png");
+            File.Delete(_filePath);
 
-			var settings = new Settings
-			{
-				ScreenCapturePath = path
-			};
+            var settings = new Settings
+            {
+                ScreenCapturePath = path
+            };
 
-			_session = new Session<InternetExplorer>(settings);
-			_session.NavigateTo<CheckboxPage>(GetUrl("Checkbox.html"));
-			_returnSession = _session.CaptureScreen(_filePath);
-		}
+            var environment = new T();
+            _session = new Session(environment, settings);
+            _session.NavigateTo<CheckboxPage>(GetUrl("Checkbox.html"));
+            _returnSession = _session.CaptureScreen(_filePath);
+        }
 
-		[OneTimeTearDown]
-		public void After()
-		{
-			_session.End();
-			File.Delete(_filePath);
-		}
+        [OneTimeTearDown]
+        public void After()
+        {
+            _session.End();
+            File.Delete(_filePath);
+        }
 
-		[Test]
-		public void should_save_in_path()
-		{
-			File.Exists(_filePath).Should().BeTrue();
-		}
+        [Test]
+        public void Should_save_in_path()
+        {
+            File.Exists(_filePath).Should().BeTrue();
+        }
 
-		[Test]
-		public void should_return_session()
-		{
-			_returnSession.Should().Be(_session);
-		}
-	}
+        [Test]
+        public void Should_return_session()
+        {
+            _returnSession.Should().Be(_session);
+        }
+    }
 }
