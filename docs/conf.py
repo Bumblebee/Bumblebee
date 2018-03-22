0cef42e277e3f893f314d57826aae38bd8d0737c
Todd Meinershagen <todd@meinershagen.net>
2018-03-22 00:56:46 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -169,8 +169,6 @@ texinfo_documents = [
 # app setup hook
 def setup(app):
     app.add_config_value('recommonmark_config', {
-        'url_resolver': lambda url: github_doc_root + url,
-        'enable_eval_rst': True,
         'enable_auto_doc_ref': True,
     }, True)
     app.add_transform(AutoStructify)
