0b8870c76aca71a7c98598d7f19dfa86e5c46e42
Todd Meinershagen <todd@meinershagen.net>
2018-03-22 01:03:33 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
 import recommonmark
 from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
